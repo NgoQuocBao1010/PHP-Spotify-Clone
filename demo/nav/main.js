c6cf37cbb0b5d518b2ed4b48d2192c2e9a84f0ad
Bao <totti.chelsea@gmail.com>
2021-06-02 20:53:24 +0700

    powerpoitn entity diagram

--- demo/nav/main.js
+++
@@ -1,6 +0,0 @@
-const profilePic = document.querySelector(".profilePic img");
-const links = document.querySelector(".links");
-
-profilePic.addEventListener("click", () => {
-    links.classList.toggle("active");
-});
--- userRegis/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // Database connection 
-    $conn = mysqli_connect('localhost', 'root', '', 'school');
-    mysqli_set_charset($conn, "utf8");
-    if (!$conn) {
-        echo mysqli_connect_error();
-    }
-?>
