526a2673e70c614b5fd6939ada01751477d2ac58
Bao <totti.chelsea@gmail.com>
2021-06-26 17:03:45 +0700

    ...

--- QuocBao-PhuNguyen/src/favourites.php
+++
@@ -1,8 +0,0 @@
-<?php include("./index.php") ?>
-<script>
-    if (!authenticated) {
-        loginPopup();
-        window.history.pushState("", "", pageUrl + "/");
-    } else
-        showContent("favourites");
-</script>
--- QuocBao-PhuNguyen/src/utils/dbConnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database connection 
-$conn = mysqli_connect('localhost:3307', 'root', '', 'myspotify');
-mysqli_set_charset($conn, "utf8");
-if (!$conn) {
-    echo mysqli_connect_error();
-}
