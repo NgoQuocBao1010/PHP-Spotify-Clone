c617cfe3ff0259cddebbd111fe70118e03552e63
NgoQuocBao1010 <totti.chelsea@gmail.com>
2022-01-26 14:42:37 +0700

    🆕 feat: mysql connection config for docker

--- main/src/utils/dbConnection.php
+++ main/src/utils/dbConnection.php
@@ -1,7 +1,9 @@
 <?php
 // Database connection 
-$conn = mysqli_connect('localhost:8083', 'root', 'user', 'myspotify');
+$conn = mysqli_connect('db', 'root', 'root', 'myspotify');
+
 mysqli_set_charset($conn, "utf8");
+
 if (!$conn) {
     echo mysqli_connect_error();
 }
